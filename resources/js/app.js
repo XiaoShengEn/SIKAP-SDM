c4c9bb1d9e0c84eedfc79c0658dd5921d68d4bf1
dkatonn <dimasberightback@gmail.com>
2026-02-26 05:27:54 +0000

    Fix realtime via server

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,34 +10,19 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
+
 function dispatchBrowserEvent(name, detail) {
     window.dispatchEvent(new CustomEvent(name, { detail }));
 }
 
-const browserHost = window.location.hostname;
-const reverbHost = import.meta.env.VITE_REVERB_HOST || browserHost;
-const reverbPort = import.meta.env.VITE_REVERB_PORT ?? 8081;
-const reverbScheme = import.meta.env.VITE_REVERB_SCHEME || window.location.protocol.replace(':', '');
-
-// window.Echo = new Echo({
-//     broadcaster: 'reverb',
-//     key: import.meta.env.VITE_REVERB_APP_KEY,
-//     wsHost: import.meta.env.VITE_REVERB_HOST,
-//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 80,
-//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
-//     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
-//     enabledTransports: ['ws', 'wss'],
-//     disableStats: true,
-// });
-
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: reverbHost,
-    wsPort: reverbPort,
-    wssPort: reverbPort,
-    forceTLS: reverbScheme === 'https',
-    enabledTransports: ['ws', 'wss'],
+    key: 'local',
+    wsHost: window.location.hostname,
+    wsPort: 8081,
+    wssPort: 8081,
+    forceTLS: false,
+    enabledTransports: ['ws'],
     disableStats: true,
 });
 
--- resources/js/echo.js
+++ resources/js/echo.js
@@ -1,15 +1,9 @@
 import Echo from 'laravel-echo'
-import Pusher from 'pusher-js'
+
 
 
 
 window.Echo = new Echo({
     broadcaster: 'reverb',
-    client: new Pusher(import.meta.env.VITE_REVERB_APP_KEY, {
-        wsHost: import.meta.env.VITE_REVERB_HOST,
-        wsPort: import.meta.env.VITE_REVERB_PORT,
-        wssPort: import.meta.env.VITE_REVERB_PORT,
-        forceTLS: false,
-        enabledTransports: ['ws'],
-    }),
+    host: window.location.hostname + ':8081',
 });
